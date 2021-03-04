e7738094118060821881d94f050907585be9ca75
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-03-04 15:17:48 +0100

    Util functions to format tweets get tweet by id get tweets from specified user timeline extract media links

--- daily_tip/twitterapp/utils.py
+++ daily_tip/twitterapp/utils.py
@@ -1,5 +1,7 @@
 import tweepy
 import os
+import json 
+import pprint
 
 consumer_key = os.environ.get('TWITTER_API_KEY')
 consumer_secret = os.environ.get('TWITTER_API_SECRET')
@@ -13,3 +15,55 @@ def tweepy_api_auth():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     return api
+
+
+def format_response(response):
+    #convert to string
+    json_str = json.dumps(response)
+    #deserialise string into python object
+    parsed = json.loads(json_str)
+    # print(json.dumps(parsed, indent=4, sort_keys=True))
+    return parsed
+
+
+def get_tweet(id):
+   api = tweepy_api_auth()
+   tweet = api.get_status(id)
+#    print("tweet", tweet)
+   tweet = tweet._json
+   return format_response(tweet)
+
+def get_timeline_tweets(user, count):
+    tweets = api.user_timeline(screen_name=user,
+                                    count=count,
+                                    include_rts=False,
+                                    tweet_mode='extended')
+    tweets = tweets[0]._json # extract json part of tweepy response
+    
+    return format_response(tweets)
+
+
+def extract_media_links(tweet):
+    """
+    :tweet single tweet object/dict
+    :return turple (boolean, obj)
+    """
+    link_dict = {}
+    has_link = False
+    links = []
+    try:
+        media = tweet['entities']['media']
+        for item in media:
+            links.append(item['media_url_https'])
+        has_link = True 
+        link_dict['id'] = tweet['id']
+        link_dict['links'] = links
+    except KeyError:
+        return has_link, link_dict
+    
+    return has_link, link_dict
+
+
+
+
+
--- daily_tip/twitterapp/views.py
+++ daily_tip/twitterapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import tweepy
 from django.views import View
-from twitterapp.utils import tweepy_api_auth
-import pprint
+from twitterapp.utils import tweepy_api_auth, format_response, get_tweet, extract_media_links
+
 
 # Create your views here.
 class TwitterView(View):
@@ -12,7 +12,16 @@ class TwitterView(View):
     user_id = 'python_tip'
 
     def get(self, request):
+        api = self.api
+        user = self.user_id
         # get the last 200 tweets 
+        # format_response(tweets)
+        
+        # print(get_tweet('1362776037428502534'))
+
+
+        extract_media_links(get_tweet("1356937628566880260"))
+
         return HttpResponse("<h2> Twitter </h2>")
 
 
