243f23027f587b964b8986835b11bb987683790e
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-03-04 11:58:52 +0100

    Twitter auth with keys and secret

--- daily_tip/twitterapp/views.py
+++ daily_tip/twitterapp/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import tweepy
 from django.views import View
+from twitterapp.utils import tweepy_api_auth
+import pprint
 
 # Create your views here.
 class TwitterView(View):
+
+    api = tweepy_api_auth()
+    user_id = 'python_tip'
+
     def get(self, request):
-        
+        # get the last 200 tweets 
         return HttpResponse("<h2> Twitter </h2>")
 
+
+class TwitterAuth(View):
+    pass
