20b31911bf95b7fe9da6e6895b5c3c1358b2371c
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-03-08 01:42:38 +0100

    Configured twitterapp to run at server startup instead of wrong previously configured tips app

--- daily_tip/tips/apps.py
+++ daily_tip/tips/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TipsConfig(AppConfig):
     name = 'tips'
+
--- daily_tip/twitterapp/apps.py
+++ daily_tip/twitterapp/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class TwitterConfig(AppConfig):
     name = 'twitterapp'
+
+    def ready(self):
+        from twitterapp import updater
+        updater.start()
